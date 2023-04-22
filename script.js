de66cc1030f41f82a670a70a08628bd436e0ad41
Chandan268 <114098024+Chandan268@users.noreply.github.com>
2023-04-23 04:16:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let MenuBtn= document.getElementById('MenuBtn')
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('bi-x')
+})
+
+
+let typed= new Typed('.auto-input',{
+    strings:['Full-Stack Developer!','Full-Stack Developer!','MERN Stack-Developer!' ],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+
+
+})
+
+let navLinks = document.querySelectorAll('nav ul li a')
+
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function (){
+    const scrollPos = this.window.scrollY
+    sections.forEach(section=>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link=>{
+                link.classList.remove('active');
+                if(section.getAttribute('id')== link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
